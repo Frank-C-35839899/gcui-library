6290fab4e00bf52ef4276607e119f6c0836bd961
Frank-C-35839899 <gcui0005@student.monash.edu>
2026-08-19 20:22:51 +1000

    W3S6

--- src/components/LibraryRegistrationForm.vue
+++ src/components/LibraryRegistrationForm.vue
@@ -4,7 +4,7 @@
       <div class="col-md-8 offset-md-2">
         <h1 class="text-center">User Information Form</h1>
 
-        <form>
+        <form @submit.prevent="submitForm">
           <div class="row mb-3">
             <div class="col-md-6">
               <label for="username" class="form-label">Username</label>
@@ -12,6 +12,7 @@
                 type="text"
                 class="form-control"
                 id="username"
+                v-model="formData.username"
               />
             </div>
 
@@ -21,6 +22,7 @@
                 type="password"
                 class="form-control"
                 id="password"
+                v-model="formData.password"
               />
             </div>
           </div>
@@ -28,14 +30,26 @@
           <div class="row mb-3">
             <div class="col-md-6">
               <div class="form-check">
-                <input type="checkbox" />
-                <label>Australian Resident?</label>
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="isAustralian"
+                  v-model="formData.isAustralian"
+                />
+                <label class="form-check-label" for="isAustralian">
+                  Australian Resident?
+                </label>
               </div>
             </div>
 
             <div class="col-md-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender">
+              <select
+                class="form-select"
+                id="gender"
+                v-model="formData.gender"
+              >
+                <option value="">Select Gender</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
@@ -47,32 +61,126 @@
             <label for="reason" class="form-label">
               Reason for joining
             </label>
+
             <textarea
               class="form-control"
               id="reason"
               rows="3"
+              v-model="formData.reason"
             ></textarea>
           </div>
 
           <div class="text-center">
-            <button type="submit" class="btn btn-primary me-2">
+            <button
+              type="submit"
+              class="btn btn-primary me-2"
+            >
               Submit
             </button>
 
-            <button type="button" class="btn btn-secondary">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="clearForm"
+            >
               Clear
             </button>
           </div>
         </form>
       </div>
     </div>
+
+    <!-- Submitted Cards -->
+    <div
+      class="row mt-5"
+      v-if="submittedCards.length"
+    >
+      <div class="d-flex flex-wrap justify-content-start">
+
+        <div
+          v-for="(card, index) in submittedCards"
+          :key="index"
+          class="card m-2"
+          style="width: 18rem;"
+        >
+          <div class="card-header">
+            User Information
+          </div>
+
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              Username: {{ card.username }}
+            </li>
+
+            <li class="list-group-item">
+              Password: {{ card.password }}
+            </li>
+
+            <li class="list-group-item">
+              Australian Resident:
+              {{ card.isAustralian ? 'Yes' : 'No' }}
+            </li>
+
+            <li class="list-group-item">
+              Gender: {{ card.gender }}
+            </li>
+
+            <li class="list-group-item">
+              Reason: {{ card.reason }}
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const formData = ref({
+  username: '',
+  password: '',
+  isAustralian: false,
+  reason: '',
+  gender: ''
+})
+
+const submittedCards = ref([])
+
+const submitForm = () => {
+  submittedCards.value.push({
+    ...formData.value
+  })
+}
+
+const clearForm = () => {
+  formData.value = {
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+  }
+}
 </script>
 
 <style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
+.card-header {
+  background-color: #275FDA;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+
+.list-group-item {
+  padding: 10px;
+}
 </style>
